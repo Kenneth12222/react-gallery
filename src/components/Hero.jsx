e014e67ad1ad4bef5c5839f4a144f2b4d29e4693
Kenneth Mburu <kenneth.mburu@student.moringaschool.com>
2024-04-17 08:17:28 +0300

    hero-conponents..

---
+++ src/components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Hero.css'
+
+function Hero() {
+  return (
+    <div>
+      <div className='hero'>
+        <div className='content'>
+          <h1> 
+          The best free stock photos, royalty free images & videos shared by creators
+          </h1>
+
+          <form>
+            <input type='text' placeholder='Search now' />
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
